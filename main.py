960c57d550f457a86c78261ff90774c1cb0c1e5e
ladettanguy <sti2dlab.ladettanguy@gmail.com>
2022-12-20 13:48:59 +0100

    test

---
+++ main.py
@@ -0,0 +1,6 @@
+def calculate(x, y):
+    return x + y
+
+
+if __name__ == '__main__':
+    calculate(1, 2)
---
+++ test/test_main.py
@@ -0,0 +1,5 @@
+from main import calculate
+
+
+def test_calculate():
+    assert calculate(1, 2) == (1 + 2)
